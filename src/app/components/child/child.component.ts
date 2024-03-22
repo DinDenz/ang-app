1f115ca00af69978634b1bb21896dd6c437af6f0
DinDenz <DinDenz@users.noreply.github.com>
2024-03-22 15:55:02 +0300

    ts11

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
 
+interface User {
+  id : number;
+  name : string;
+  font : string;
+}
+
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrl: './child.component.scss'
 })
 export class ChildComponent {
+  newClass: boolean = true;
+  secClass: boolean = false;
 
+  changeClass (event : Event) : void {
+    this.newClass = false;
+    this.secClass = true;
+  }
+
+  expression = true;
+  changeExpression() :void {
+    this.expression = !this.expression;
+  }
+
+  deepObject : any = null;
+
+  generateUser () {
+    this.deepObject = {
+      user : {
+        name : "Ivan",
+        surname : "Petrov",
+      }
+    }
+  }
+
+  users = [
+    {
+      id : 1,
+      name: "Petr",
+      font : "italic",
+    },
+    {
+      id : 2,
+      name: "Ivan",
+      font : "bold",
+    },
+    {
+      id : 3,
+      name: "Alexey",
+      font : "underline",
+    }
+  ]
+  trackBy (index : number, user : User) {
+    return user.id //так мы трекаем цикл по айдишникам(а ля кеу???)
+  }
 }
